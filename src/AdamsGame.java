c9fe76ee11ad146a659bce165d6af5527c356806
league <league@imac-18.attlocal.net>
2022-06-20 16:54:44 -0700

    I worked on my game today

--- src/AdamsGame.java
+++ src/AdamsGame.java
@@ -1,9 +1,30 @@
+import javax.swing.JFrame;
 
 public class AdamsGame {
 
+	public static final int WIDTH = 500;
+    public static final int HEIGHT = 800;
+    JFrame frame;
+    GamePanel panel = new GamePanel();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		
+		new AdamsGame().setup();
+		
+		
+		
+	}
+	
+	AdamsGame() {
+		frame = new JFrame();
+	}
+	void setup() {
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(panel);
+		
+		            
 	}
 
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,76 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
 
-}
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	Font titleFont= new Font("Arial", Font.PLAIN, 48);
+    Font enterFont= new Font("Arial", Font.PLAIN, 24);
+    final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState= MENU;
+    
+    GamePanel() {
+    	repaint();
+    }
+    
+    
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.GRAY);
+		g.fillRect(0, 0, AdamsGame.WIDTH, AdamsGame.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.GREEN);
+		g.drawString("NUMBER WARS", 65, 250);
+		g.setFont(enterFont);
+		g.drawString("Press ENTER to start", 115, 500);
+		g.drawString("Press SPACE for instructions", 80, 750);
+		
+	}
+private void drawEndState(Graphics g) {
+		
+		
+	}
+	private void drawGameState(Graphics g) {
+		
+		
+	}
+
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}
+		else if(currentState == GAME){
+		    drawGameState(g);
+		}
+		else if(currentState == END){
+		    drawEndState(g);
+		}
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}      
